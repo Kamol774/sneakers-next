2b2f77b4ee025d652c26df82d6703634606bac31
Kamol774 <chust1222@gmail.com>
2024-07-10 13:59:35 +0500

    fix: create related folders

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,6 +1,8 @@
 import { Stack } from "@mui/material";
 import { green } from "@mui/material/colors";
 import Head from "next/head";
+import Top from "../Top";
+import Footer from "../Footer";
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
@@ -10,13 +12,18 @@ const withLayoutMain = (Component: any) => {
           <title>Sneakers</title>
         </Head>
         <Stack id="pc-wrap">
-          <Stack sx={{ background: "#81c784" }}>Header Home</Stack>
+          <Stack id={"top"}>
+            <Top />
+          </Stack>
+          <Stack className={"header-main"}>Header Filter</Stack>
 
           <Stack id={"main"}>
             <Component {...props} />
           </Stack>
 
-          <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
+          <Stack id={"footer"}>
+            <Footer />
+          </Stack>
         </Stack>
       </>
     );
